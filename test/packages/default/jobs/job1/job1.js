b634dfc37ef663110c9c0392dc2490e232b70810
Iván Loire <ivan@iloire.com>
2013-03-07 00:31:34 +1100

    Added support for namespaced jobs

--- test/packages/default/jobs/job1/job1.js
+++ test/packages/default/jobs/job1/job1.js
@@ -1,1 +1,3 @@
-module.exports = function(widgets, config, dependencies) {};
+module.exports = function(widgets, config, dependencies) {
+	return "default#job1"; //return this for testing purposes so we can identify this job location
+};
---
+++ test/packages/otherpackage1/jobs/job1/job1.js
@@ -0,0 +1,3 @@
+module.exports = function(widgets, config, dependencies) {
+	return "otherpackage1#job1"; //return this for testing purposes so we can identify this job location
+};
