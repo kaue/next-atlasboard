b3d53deab4f6e91e8351fec6fe25e10ee1b0d368
Iván Loire <ivan@iloire.com>
2013-03-08 18:38:30 +1100

    Added logger dependency

--- packages/atlasboard/jobs/picture-of-the-day/picture-of-the-day.js
+++ packages/atlasboard/jobs/picture-of-the-day/picture-of-the-day.js
@@ -1,13 +1,14 @@
 var $ = require('cheerio');
 
 module.exports = function(widgets, config, dependencies) {
+    var logger = dependencies.logger;
     var options = {
         url: config.url
     };
 
     dependencies.request(options, function(err, response, body) {
         if (err || !response || response.statusCode != 200) {
-            console.error(err || "ERROR: Couldn't access the web page at %s", options.url);
+            logger.error(err || "ERROR: Couldn't access the web page at " + options.url);
             widgets.sendData({error: "error loading picture" });
         } else {
             var result = $('.primary_photo img', body).attr('src');
