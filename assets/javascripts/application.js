27a3d0046f3e53ad7a1ae519ed50931be2fb3cbd
Iván Loire <ivan@iloire.com>
2013-03-12 20:02:59 +1100

    Fade out widget if error or offline

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -16,17 +16,21 @@ $(function() {
       if (lastUpdate){
         var elapsed = ((+new Date()) - lastUpdate) / 1000;
         var str_elapsed = '';
+        var offline = false;
 
         if (elapsed > 60 * 60){
           str_elapsed = ' <span class="alert alert_high">&gt;1h</span>';
         }
         else if (elapsed > 20 * 60){
+          offline = true;
           str_elapsed = ' <span class="alert alert_high">&gt;20m</span>';
         }
         else if (elapsed > 10 * 60){
+          offline = true;
           str_elapsed = ' <span class="alert alert_low">&gt;10m</span>';
         }
         else if (elapsed > 5 * 60){
+          offline = true;
           str_elapsed = ' <span class="alert alert_normal">&gt;5m</span>';
         }
 
@@ -34,19 +38,36 @@ $(function() {
           $('.widget-title', li).append('<span class="widget-elapsed"></span>');
         }
         $('.widget-title span.widget-elapsed', li).html(str_elapsed);
+
+        if (offline){ // fade widget
+          $(li).addClass('offline');
+        }
+        else{
+          $(li).removeClass('offline');
+        }
       }
 
     });
   }
 
-  var defaultHandlers = {
+  var defaultHandlers = { //they can be overwritten by widget´s custom implementation
     onError : function (el, data){
       $('.content', el).html("<div class='error'>" + data.error + "</div>");
     },
     onInit : function (el, data){
       $("<img class=\"spinner\" src=\"images/spinner.gif\">").insertBefore($('.content', el));
+    }
+  };
+
+  var globalHandlers = { //global pre-post event handlers
+    onPreError : function (el, data){
+      console.log('err');
+      $(el).addClass('onerror');
+      $(".spinner", el).hide();
     },
+
     onPreData : function (el, data){
+      $(el).removeClass('onerror');
       $(".spinner", el).hide();
     }
   };
@@ -65,13 +86,17 @@ $(function() {
       $.get("/widgets/" + widgetId + '/js', function(js) {
 
         var widget_js = eval(js);
+
         $.extend(widget_js, defaultHandlers);
 
         widget_js.onInit(li);
 
         widgetsSocket.on(eventId, function (data) { //bind socket.io event listener
             var f = data.error ? widget_js.onError : widget_js.onData;
-            defaultHandlers.onPreData(li);
+            if (data.error){
+              globalHandlers.onPreError($(li), data);
+            }
+            globalHandlers.onPreData(li);
             f($(li), data);
 
             // save timestamp
