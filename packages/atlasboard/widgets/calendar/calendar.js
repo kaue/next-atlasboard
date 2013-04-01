044de124eaecacb107c4aebbf9f01d0e1a64ce55
Ivan Loire <ivan@iloire.com>
2013-04-01 21:53:17 +1100

    real-time server and client side logging visualization using socket.io

--- packages/atlasboard/widgets/calendar/calendar.js
+++ packages/atlasboard/widgets/calendar/calendar.js
@@ -9,6 +9,9 @@ Widgets.calendar = {
         if (!data.events || !data.events.length) {
             $('.content', el).append($("<div>").html("No events found."));
         } else {
+            
+            this.log (data.events.length + ' calendar events found!'); 
+
             data.events.forEach(function (event) {
                 var eventDiv = $("<div/>").addClass('leave-event');
                 $(eventDiv).append($("<div/>").addClass('leave-dates').append(event.startDate + " - " + event.endDate));
