a110fac12ed28233c7e7521904d0b075adce884c
Ivan Loire <iloire@atlassian.com>
2013-05-06 11:25:28 +1000

    Issue #33 - Throw error and exit if http server port is in use or it is not accesible

--- test/test-atlasboard.js
+++ test/test-atlasboard.js
@@ -4,8 +4,23 @@ var atlasboard = require ('../lib/atlasboard');
 describe ('atlasboard', function(){
   describe ('start', function(){
     it('should start without errors', function(done){
-      atlasboard();
+      atlasboard(4000);
       done();
     });
+
+    it('should throw error if port is not specified', function(done){
+      //http://www.adaltas.com/blog/2013/03/27/test-uncaughtException-error-mocha/
+      var list = process.listeners ('uncaughtException');
+      process.removeAllListeners('uncaughtException');
+      process.on('uncaughtException', function (error) {
+        process.removeAllListeners('uncaughtException');
+        for (var i = list.length - 1; i >= 0; i--) {
+          process.on('uncaughtException', list[i]);
+        }
+        done();
+      });
+      atlasboard();
+    });
+
   });
 });
