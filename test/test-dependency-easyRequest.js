92af184ebf75f6b4797d7ffe1f9260344fddd0fe
Ivan Loire <iloire@atlassian.com>
2013-11-01 13:24:47 +1100

    Add HTML method to easyRequest

--- test/test-dependency-easyRequest.js
+++ test/test-dependency-easyRequest.js
@@ -7,46 +7,79 @@ describe ('dependency easyRequest', function(){
   var options, easyRequest;
 
   beforeEach(function(done){
+    nock.cleanAll();
     options = { url : 'http://invalid/' };
     easyRequest = require('../lib/job-dependencies/easyRequest/dependency')();
     done();
   });
 
-  it('should handle non 200 status code', function(done){
-    nock('http://invalid').get('/').reply(404, {});
-  
-    easyRequest.JSON(options, function(err, data){
-      assert.ok(err);
-      done();
+  describe ('JSON', function(){
+    it('should handle non 200 status code', function(done){
+      nock('http://invalid').get('/').reply(404, {});
+    
+      easyRequest.JSON(options, function(err, data){
+        assert.ok(err);
+        done();
+      });
+    });
+
+    it('should handle empty responses', function(done){
+      nock('http://invalid').get('/').reply(500, "");
+
+      easyRequest.JSON(options, function(err, data){
+        assert.ok(err);
+        done();
+      });
+    });
+
+    it('should handle non JSON responses', function(done){
+      nock('http://invalid').get('/').reply(200, "this is not json, is it?");
+
+      easyRequest.JSON(options, function(err, data){
+        assert.ok(err);
+        assert.equal('invalid json response', err);
+        done();
+      });
+    });
+
+    it('should handle successful responses', function(done){
+      nock('http://invalid').get('/').reply(200, { attr : 'some data'});
+
+      easyRequest.JSON(options, function(err, data){
+        assert.ifError(err);
+        assert.equal('some data', data.attr);
+        done();
+      });
     });
   });
 
-  it('should handle empty responses', function(done){
-    nock('http://invalid').get('/').reply(500, "");
-
-    easyRequest.JSON(options, function(err, data){
-      assert.ok(err);
-      done();
+  describe ('HTML', function(){
+    it('should handle non 200 status code', function(done){
+      nock('http://invalid').get('/').reply(404, "not found");
+    
+      easyRequest.HTML(options, function(err, data){
+        assert.ok(err);
+        done();
+      });
     });
-  });
 
-  it('should handle non JSON responses', function(done){
-    nock('http://invalid').get('/').reply(200, "this is not json, is it?");
+    it('should handle empty responses', function(done){
+      nock('http://invalid').get('/').reply(500, "");
 
-    easyRequest.JSON(options, function(err, data){
-      assert.ok(err);
-      assert.equal('invalid json response', err);
-      done();
+      easyRequest.HTML(options, function(err, data){
+        assert.ok(err);
+        done();
+      });
     });
-  });
 
-  it('should handle successful responses', function(done){
-    nock('http://invalid').get('/').reply(200, { attr : 'some data'});
+    it('should handle successful responses', function(done){
+      nock('http://invalid').get('/').reply(200, "<h2>hello</h2>");
 
-    easyRequest.JSON(options, function(err, data){
-      assert.ifError(err);
-      assert.equal('some data', data.attr);
-      done();
+      easyRequest.HTML(options, function(err, data){
+        assert.ifError(err);
+        assert.equal('<h2>hello</h2>', data);
+        done();
+      });
     });
   });
 
