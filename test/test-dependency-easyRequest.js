d918896e63255d9c58386d588fe49bcfb0603147
Ivan Loire <iloire@atlassian.com>
2013-11-01 13:03:27 +1100

    Add easyRequest dependency

---
+++ test/test-dependency-easyRequest.js
@@ -0,0 +1,53 @@
+var assert = require ('assert'),
+    path = require('path'),
+    nock = require('nock');
+ 
+describe ('dependency easyRequest', function(){
+
+  var options, easyRequest;
+
+  beforeEach(function(done){
+    options = { url : 'http://invalid/' };
+    easyRequest = require('../lib/job-dependencies/easyRequest/dependency')();
+    done();
+  });
+
+  it('should handle non 200 status code', function(done){
+    nock('http://invalid').get('/').reply(404, {});
+  
+    easyRequest.JSON(options, function(err, data){
+      assert.ok(err);
+      done();
+    });
+  });
+
+  it('should handle empty responses', function(done){
+    nock('http://invalid').get('/').reply(500, "");
+
+    easyRequest.JSON(options, function(err, data){
+      assert.ok(err);
+      done();
+    });
+  });
+
+  it('should handle non JSON responses', function(done){
+    nock('http://invalid').get('/').reply(200, "this is not json, is it?");
+
+    easyRequest.JSON(options, function(err, data){
+      assert.ok(err);
+      assert.equal('invalid json response', err);
+      done();
+    });
+  });
+
+  it('should handle successful responses', function(done){
+    nock('http://invalid').get('/').reply(200, { attr : 'some data'});
+
+    easyRequest.JSON(options, function(err, data){
+      assert.ifError(err);
+      assert.equal('some data', data.attr);
+      done();
+    });
+  });
+
+});
