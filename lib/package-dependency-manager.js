kaue/next-atlasboard

commit e0c6120084dac0501257580117ad81266ce99ca1

Install only production dependencies on atlasboard start

lib/package-dependency-manager.js
@@ -89,7 +89,7 @@ function install (pathPackageJson, callback){
   process.chdir(pathPackageJson);
 
   console.log ('\nChecking npm dependencies for ' + pathPackageJson + '...');
-  executeCommand('npm', ["install", pathPackageJson], function(err, code){
+  executeCommand('npm', ["install", ,"--production", pathPackageJson], function(err, code){
     if (err){
       callback('Error installing dependencies for ' + pathPackageJson + '. err:' + err);
     } else {

commit d94d1c023c495ee4ec6652e4b689c9c43d021e84

Fix bug introduced with e0c6120

lib/package-dependency-manager.js
@@ -89,7 +89,7 @@ function install (pathPackageJson, callback){
   process.chdir(pathPackageJson);
 
   console.log ('\nChecking npm dependencies for ' + pathPackageJson + '...');
-  executeCommand('npm', ["install", ,"--production", pathPackageJson], function(err, code){
+  executeCommand('npm', ["install", "--production", pathPackageJson], function(err, code){
     if (err){
       callback('Error installing dependencies for ' + pathPackageJson + '. err:' + err);
     } else {