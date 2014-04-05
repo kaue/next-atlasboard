bdf222c4abb87c4781e1da769f8d23d807d511d5
Ivan Loire <iloire@atlassian.com>
2014-04-05 11:52:22 +1100

    Resolve issue #67 - Resize fonts according to widget width

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -5,6 +5,13 @@
 
 $(function() {
 
+  function resizeBaseLineBasedOnWidgetWith ($el){
+    var RATIO = 0.035; // ratio width vs font-size. i.e: for a 400 width widget, 400 * 0.035 = 14px font size as baseline
+    var divWidth = $el.width();
+    var newFontSize = divWidth * RATIO;
+    $('.content.auto-font-resize', $el).css({"font-size" : newFontSize + 'px'});
+  }
+
   //----------------------
   // Alert for timeouts
   //----------------------
@@ -44,6 +51,7 @@ $(function() {
     onInit : function (el, data){
       $(el).parent().children().hide();
       $(el).parent().children(".spinner").show();
+      resizeBaseLineBasedOnWidgetWith($(el));
     }
   };
 
