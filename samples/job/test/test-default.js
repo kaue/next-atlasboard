2ee3bd79d1201604033c61fb342bd718a6588d83
Ivan Loire <iloire@atlassian.com>
2014-07-14 00:15:52 +1000

    Extract and improve project and component scaffolding
    - Now we can generate items from subfolders (ex: job tests) and run the ejs templating engine on them.
    - The job sample got better.
    - Also added unit-test template file.
    - The project default files can be ejs'ed as well.

---
+++ samples/job/test/test-default.js
@@ -0,0 +1,68 @@
+/**
+ * Test file for Job: newcalendar
+ */
+
+var assert = require ('assert');
+<%=name%>SUT = require('../<%=name%>');
+
+var mockedConfig, mockedDependencies;
+
+describe ('<%=name%> test', function(){
+
+  beforeEach(function (done) {
+    
+    // you may probably need a mocked configuration object like this to unit test configuration
+    // parameter check
+    mockedConfig = {
+      globalAuth: {
+        myconfigKey: {
+          username: "myusername",
+          password: "secretpassword"
+        }
+      },
+      interval: 20000
+    };
+
+    // mocking easyRequest in order to unit test this job (check the test below)
+    mockedDependencies = {
+      logger: console,
+      easyRequest : {
+        JSON : function (options, cb) {
+          cb(null, {});
+        }
+      }
+    };
+
+    done();
+  });
+
+  it('should have tests :D', function (done){
+    assert.ok(false, 'we don\'t have any tests :(');
+    done();
+  });
+
+  describe ('config checks', function(){
+    it('should check for valid credentials', function (done){
+      // there are some nice examples of how to unit tests configuration handling
+      // in the Atlassian package:
+      // https://bitbucket.org/atlassian/atlasboard-atlassian-package/src/master/jobs
+      done();
+    });
+  });
+
+  describe ('http request example tests', function(){
+    it('should handle server errors', function (done){
+
+      mockedDependencies.easyRequest.HTML = function (options, cb){
+        cb(null, 'hello from google');
+      };
+
+      var config = {};
+      <%=name%>SUT(config, mockedDependencies, function(err, data){
+        assert.equal('hello from google', data.html, 'expected reply from goodle but got ' + data.html);
+        done();
+      });
+    });
+  });
+
+});
--- samples/project/README.md
+++ samples/project/README.md
@@ -1,2 +1,2 @@
-# My Awesome Atlasboard Dashboard
+# <%=name %>, my awesome Atlasboard dashboard
 This is the README for my dashboard. Any info I need to pass on to other users will go here.
