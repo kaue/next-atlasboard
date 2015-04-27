fed7e15ee87cae8c189851ea2c1ab024a8a0115c
Ivan Loire [Atlassian] <iloire@atlassian.com>
2015-04-27 11:42:52 +0000

    Remove extra space

--- test/test-template-manager.js
+++ test/test-template-manager.js
@@ -2,7 +2,7 @@ var assert = require ('assert');
 var path = require ('path');
 var templateManager = require('../lib/template-manager');
 
-describe ('template manager', function(){
+describe('template manager', function(){
 
   it('should resolve global location of template', function(done) {
     templateManager.resolveTemplateLocation("dashboard.ejs", function(err, location){
