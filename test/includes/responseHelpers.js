fb61819e85df5ea975b3c1f59b75e004e5d3108d
Ivan Loire <iloire@atlassian.com>
2015-08-10 10:21:34 +1000

    Add support for both stylus and CSS in the same widget. Refactor tests

---
+++ test/includes/responseHelpers.js
@@ -0,0 +1,104 @@
+var assert = require ('assert');
+
+exports = module.exports = (function(){
+
+  function getResponseForSendFile (expectedFileContains, done){
+    var res = {
+      type: function (data){
+        assert.equal("application/javascript", data);
+      },
+
+      sendfile: function (file){
+        assert.ok(file.indexOf(expectedFileContains)>-1);
+        done();
+      },
+
+      write: function (data){
+        done("not expected");
+      },
+
+      end: function (data){
+        done("not expected");
+      }
+    };
+
+    return res;
+  }
+
+  function getResponseForSendStatusCode (statusCode, done){
+    var res = {
+      type: function (data){
+
+      },
+
+      sendfile: function (){
+        done('not expected call to res.sendfile');
+      },
+
+      write: function (){
+        done("not expected call to res.write");
+      },
+
+      end: function (){
+        done("not expected call to res.end");
+      },
+
+      send: function (status, data){
+        assert.equal(statusCode, status);
+        done();
+      }
+    };
+    return res;
+  }
+
+  function getResponseWriteEnd (contains, mime, done){
+    var bundle_file = "";
+    var res = {
+      type: function (data){
+        if (mime){
+          assert.equal(mime, data);
+        }
+      },
+
+      sendfile: function (file){
+        done("not expected");
+      },
+
+      write: function (data){
+        bundle_file+=data;
+      },
+
+      end: function (data){
+        if (contains){
+          if (!Array.isArray(contains)){
+            contains = [contains];
+          }
+          contains.forEach(function(match){
+            console.log(bundle_file)
+            assert.ok(bundle_file.indexOf(match) > -1);
+          });
+        }
+        done();
+      }
+    };
+    return res;
+  }
+
+  function getResponseWriteBasic() {
+    var res = {
+      written: '',
+      write: function (data) {
+        res.written += data;
+      }
+    };
+    return res;
+  }
+
+  return {
+    getResponseForSendFile:getResponseForSendFile,
+    getResponseForSendStatusCode: getResponseForSendStatusCode,
+    getResponseWriteEnd: getResponseWriteEnd,
+    getResponseWriteBasic: getResponseWriteBasic
+  }
+
+})();
