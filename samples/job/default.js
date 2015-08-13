f3409d3c4f1125583b4c2b966dd355bc4f7e375a
Ivan Loire <iloire@atlassian.com>
2015-08-13 17:15:55 +1000

    Rewrite job template

--- samples/job/default.js
+++ samples/job/default.js
@@ -13,33 +13,59 @@
 
 module.exports = function(config, dependencies, job_callback) {
 
-    // ## 1. USE OF DEPENDENCIES ##
-    // You can use the following dependencies in your job:
-    // - dependencies.easyRequest : a wrapper on top of the "request" module
-    // - dependencies.request : the popular http request module itself
-    // - dependencies.logger : atlasboard logger interface
-    // - dependencies.underscore
-    // - dependencies.moment
-    // - dependencies.storage : a simple persistance layer for Atlasboard    
+    /*
 
-    // # 2. CONFIGURATION CHECK
-    // You probably want to check that the right configuration has been passed to the job.
-    // You can add unit tests to ensure this (see test/<%=name%> file)
-    // Your config check may look something like this:
-    // if (!config.globalAuth || !config.globalAuth[authName] ||
-    //   !config.globalAuth[authName].username || !config.globalAuth[authName].password) {
-    //   return job_callback('no credentials found in the <%=name%> job. Please check the global authentication file!');
-    // }
+    1. USE OF JOB DEPENDENCIES
 
-    // # 3. USE OF JOB_CALLBACK
-    // Using nodejs callback standard conventions, you should return an error or null (if success) 
-    // as the first parameter, and the actual data to be sent to the widget as the second parameter. 
-    // Atlasboard will deal with the rest.
+    You can use a few handy dependencies in your job:
 
-    // This is an example of how to make an HTTP call to google using the easyRequest dependency, 
-    // and send the result to the registered atlasboard widgets.
-    // Have a look at test/<%=name%> for an example of how to unit tests this easily by mocking easyRequest calls
+    - dependencies.easyRequest : a wrapper on top of the "request" module
+    - dependencies.request : the popular http request module itself
+    - dependencies.logger : atlasboard logger interface
+
+    Check them all out: https://bitbucket.org/atlassian/atlasboard/raw/master/lib/job-dependencies/?at=master
+
+    */
+
+    var logger = dependencies.logger;
+
+    /*
+
+    2. CONFIGURATION CHECK
+
+    You probably want to check that the right configuration has been passed to the job.
+    It is a good idea to cover this with unit tests as well (see test/<%=name%> file)
+
+    Checking for the right configuration could be something like this:
+
+    if (!config.myrequiredConfig) {
+        return job_callback('missing configuration properties!');
+    }
+
+
+    3. SENDING DATA BACK TO THE WIDGET
+
+    You can send data back to the widget anytime (ex: if you are hooked into a real-time data stream and
+    don't want to depend on the job_callback triggered by the scheduler to push data to widgets)
+
+    */
+    this.pushUpdate({data: { title: config.widgetTitle, html: 'loading...' }});
+
+    /*
+
+    4. USE OF JOB_CALLBACK
+
+    Using nodejs callback standard conventions, you should return an error or null (if success)
+    as the first parameter, and the widget's data as the second parameter.
+
+    This is an example of how to make an HTTP call to google using the easyRequest dependency,
+    and send the result to the registered atlasboard widgets.
+    Have a look at test/<%=name%> for an example of how to unit tests this easily by mocking easyRequest calls
+
+    */
+
     dependencies.easyRequest.HTML('http://google.com', function(err, html){
+      logger.log(html);
       job_callback(err, { title: config.widgetTitle, html: html });
     });
 };
