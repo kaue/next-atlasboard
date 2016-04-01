9ecacb8e7564a3ce11f3fe57332cd24c0428318a
Ivan Loire <iloire@atlassian.com>
2016-04-01 15:44:52 -0500

    fix: Fix issue #130. Logger configuration not working

--- test/test-logger.js
+++ test/test-logger.js
@@ -1,11 +1,20 @@
 var assert = require('assert');
-var loggerFactory = require('../lib/logger');
+var proxyquire = require('proxyquire');
+var sinon = require('sinon');
 
 describe('logger', function () {
 
-  var logger = require('../lib/logger')();
+  function getLogger(configuration, jobMock, ioMock) {
+    var loggerFactory = proxyquire('../lib/logger', {
+      './config-manager': function () {
+        return configuration || {}
+      }
+    });
+    return loggerFactory(jobMock, ioMock);
+  }
 
   it('should contain all required method', function () {
+    var logger = getLogger();
     assert.equal('function', typeof logger.log);
     assert.equal('function', typeof logger.trace);
     assert.equal('function', typeof logger.debug);
@@ -25,8 +34,30 @@ describe('logger', function () {
         }
       };
 
-      logger = loggerFactory(jobMock, ioMock);
+      var logger = getLogger({}, jobMock, ioMock);
       logger.error('hola');
+    });
+  });
+
+  describe('read custom configuration', function () {
+    afterEach(function () {
+      if (typeof console.log.restore === 'function') {
+        console.log.restore();
+      }
+    });
+
+    it('should log trace statements if level is 1', function () {
+      sinon.stub(console, 'log');
+      var logger = getLogger({logger: {level: 1}});
+      logger.trace('test');
+      assert.ok(console.log.called);
+    });
+
+    it('should not log trace statements if level is 4', function () {
+      sinon.stub(console, 'log');
+      logger = getLogger({logger: {level: 4}});
+      logger.trace('test');
+      assert.ok(!console.log.called);
     });
   });
 });
